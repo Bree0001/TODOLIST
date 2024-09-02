4afcd8975d07419fba576552ab09df1d643e21f7
Bree0001 <warigiabrenda1@gmail.com>
2024-09-02 22:31:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let toDoListArrays = [];
+
+let toDoListHTML = '';
+
+function generateHTML(){
+toDoListHTML = '';
+for(let i = 0; i < toDoListArrays.length; i++){
+  const toDoObject = toDoListArrays[i];
+  const name = toDoObject.name;
+  const dueDate = toDoObject.dueDate;
+  const html = `<p class="paragraph-JS">
+  ${name} ${dueDate} 
+  <button onclick="toDoListArrays.splice(${i}, 1); deleteToDoItem()">Delete</button>
+  </p>`;
+  console.log(html);
+  toDoListHTML += html;
+  }
+  document.querySelector('.paragraphDiv').innerHTML = toDoListHTML;
+}
+
+function addToDoItem(){
+  let toDoItemElement = document.getElementById('toDoItem');
+  let dateSelectorElement = document.querySelector('.js-date-input');
+  const dueDate = dateSelectorElement.value;
+  const listItems = toDoItemElement.value.trim();
+  if (listItems && dueDate) {
+    toDoListArrays.push({
+      name: listItems,
+      dueDate: dueDate
+    });
+  console.log(toDoListArrays);
+  toDoItemElement.value = '';
+  dateSelectorElement.value = '';
+  generateHTML();
+}}
+
+function deleteToDoItem(index) {
+  toDoListArrays.splice(index, 1);
+  generateHTML();
+}
+const addButtonElement = document.querySelector('.addButton');
+addButtonElement.addEventListener('click', addToDoItem());
+const toDoItemElement = document.getElementById('toDoItem');
+toDoItemElement.addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    addToDoItem();
+  }
+});
+
+generateHTML();
